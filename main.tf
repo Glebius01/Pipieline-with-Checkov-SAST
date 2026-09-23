c9bc140adccc5aa5dbce4bf35931f3d29aeaf279
Glebius01 <glebshepel1@gmail.com>
2026-09-23 17:35:59 +0100

    Assigned CIS baseline to the storage account

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 5.6.0"
+      version = "~> 3.0" # Note: Provider version aligned to 3.x stable
     }
   }
   required_version = ">= 1.1.0"
@@ -19,17 +19,30 @@ resource "azurerm_resource_group" "lab" {
 }
 
 resource "azurerm_storage_account" "lab" {
-  name                     = "insecurelabstorage9988" 
-  
+  name                     = "securelabstorage9988" # Globally unique name
   resource_group_name      = azurerm_resource_group.lab.name
   location                 = azurerm_resource_group.lab.location
-  
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  
-  # Vuln 1
-  public_network_access_enabled = true
-  
-  # Vuln 2
-  min_tls_version = "TLS1_0"
+}
+
+# Fetch current subscription context automatically
+data "azurerm_subscription" "current" {}
+
+# Fetch the built-in CIS Initiative definition
+data "azurerm_policy_set_definition" "cis" {
+  display_name = "CIS Microsoft Azure Foundations Benchmark v2.0.0"
+}
+
+# Assign the CIS Initiative across your entire Subscription
+resource "azurerm_subscription_policy_assignment" "cis" {
+  name                 = "lab-cis-benchmark"
+  display_name         = "Lab CIS Azure Foundations Benchmark"
+  subscription_id      = data.azurerm_subscription.current.id
+  policy_definition_id = data.azurerm_policy_set_definition.cis.id
+  location             = "Western Europe"
+
+  identity {
+    type = "SystemAssigned"
+  }
 }
